af76da3547d4f0d98534b1adf4d1d7f6cd5ea362
王天赐 <15565946702@163.com>
2022-11-26 17:01:15 +0800

    王天赐, feat: 01-创建简单的Bean容器

---
+++ README.md
@@ -0,0 +1,4 @@
+## 项目介绍
+
+
+仿写 Spring , 参考 小傅哥的 small-spring
---
+++ tiny-spring-step-01/src/main/java/cn/knightzz/BeanDefinition.java
@@ -0,0 +1,16 @@
+package cn.knightzz;
+
+/**
+ * @author 王天赐
+ * @title: BeanDefinition
+ * @projectName imitation-tiny-spring
+ * @description: 用于封装Bean对象
+ * @website <a href="http://knightzz.cn/">http://knightzz.cn/</a>
+ * @github <a href="https://github.com/knightzz1998">https://github.com/knightzz1998</a>
+ * @create: 2022-11-26 16:57
+ */
+public class BeanDefinition {
+
+    Object bean;
+
+}
