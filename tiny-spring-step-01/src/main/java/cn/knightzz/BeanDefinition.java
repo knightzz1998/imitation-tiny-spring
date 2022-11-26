622c437b6cbe78c5b098952c157b780f97b2b34b
王天赐 <15565946702@163.com>
2022-11-26 17:50:00 +0800

    王天赐, feat: 01-创建简单的Bean容器

--- tiny-spring-step-01/src/main/java/cn/knightzz/BeanDefinition.java
+++ tiny-spring-step-01/src/main/java/cn/knightzz/BeanDefinition.java
@@ -11,6 +11,13 @@ package cn.knightzz;
  */
 public class BeanDefinition {
 
-    Object bean;
+    private Object bean;
 
+    public BeanDefinition(Object bean) {
+        this.bean = bean;
+    }
+
+    public Object getBean() {
+        return bean;
+    }
 }
---
+++ tiny-spring-step-01/src/main/java/cn/knightzz/BeanFactory.java
@@ -0,0 +1,23 @@
+package cn.knightzz;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 王天赐
+ * @title: BeanFactory
+ * @projectName imitation-tiny-spring
+ * @description:
+ * @website <a href="http://knightzz.cn/">http://knightzz.cn/</a>
+ * @github <a href="https://github.com/knightzz1998">https://github.com/knightzz1998</a>
+ * @create: 2022-11-26 17:15
+ */
+public class BeanFactory {
+
+    private Map<String, BeanDefinition> beanDefinitionMaps = new HashMap<>();
+
+    public Object getBean(String beanName) {
+        return beanDefinitionMaps.get(beanName);
+    }
+
+}
